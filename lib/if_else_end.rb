4d7f415b48f7d1b4f9da0d5c5fdb7fec0fef8d9e
Tucker Brookshire <tuckerbrookshire@gmail.com>
2021-01-22 01:34:35 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,8 +1,3 @@
-chance_of_rain = -23
-puts "Let's go outside!"
-if chance_of_rain > 0.5
-  puts "Pack an umbrella!"
-else
-  puts "Enjoy the fine day!"
-end
-puts "Oh, and always wear sunscreen!"
+puts "You know what year it is??"
+this_year = 2019
+puts "Hey, it's 2019!" if this_year == 2019
