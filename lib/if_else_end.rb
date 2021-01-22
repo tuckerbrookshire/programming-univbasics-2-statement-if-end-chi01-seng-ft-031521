8d8eba2f118fdfe5b2deed312216e9aff6c27fe3
Tucker Brookshire <tuckerbrookshire@gmail.com>
2021-01-22 01:22:08 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,3 +1,6 @@
-if false
-  puts "This code does not run. You will not see this in IRB"
+run_code_inside = true
+puts "Code before if...end"
+if run_code_inside
+  puts "code inside"
 end
+puts "Code after if...end"
